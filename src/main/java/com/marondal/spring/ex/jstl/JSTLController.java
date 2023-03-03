1623fd21180346d352172abee1e1aea8cb7de847
DESKTOP-LBS7QIT\user <lecture>
2023-03-03 18:50:14 +0900

    [김인규]  - jstl fmt 라이브러리 예제

--- src/main/java/com/marondal/spring/ex/jstl/JSTLController.java
+++ src/main/java/com/marondal/spring/ex/jstl/JSTLController.java
@@ -1,6 +1,7 @@
 package com.marondal.spring.ex.jstl;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,13 +29,42 @@ public class JSTLController {
 		fruitList.add("딸기");
 		fruitList.add("바나나");
 		
-		
-		
-		
-		
 		model.addAttribute("fruitList", fruitList);
 		
+		
+		// 사용자 정보 리스트 
+		// 사용자 정보 : 이름, 나이, 취미
+		List<Map<String, Object>> userList = new ArrayList<>();
+		
+		Map<String, Object> user = new HashMap<>();
+		user.put("name", "김인규");
+		user.put("age", 28);
+		user.put("hobby", "댄스");
+		userList.add(user);
+		
+		user = new HashMap<>();
+		user.put("name", "김바다");
+		user.put("age", 5);
+		user.put("hobby", "사냥");
+		userList.add(user);
+		
+		
+		model.addAttribute("userList", userList);
+		
 		return "jstl/ex02";
+	}
+	
+	@GetMapping("/ex03")
+	public String ex03(Model model) {
+		
+		Date now = new Date();
+		
+		model.addAttribute("now", now);
+		
+		String dateString = "2023/03/03 12:11:10";
+		model.addAttribute("dateString", dateString);
+		
+		return "jstl/ex03";
 	}
 
 	
